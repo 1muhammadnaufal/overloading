dc9ca2784f111fa850eccf1bbaa01c9ca6c92fed
muhammad naufal <muhammadnaufal321@students.amikom.ac.id>
2023-04-10 20:55:51 +0700

    overloading

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+using minggu5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Overloading overloading1 = new Overloading();
+
+            int a = 82;
+            int b = 53;
+
+            int nilaiMin = overloading1.CariNilaiMin(a, b);
+            int nilaiMax = overloading1.CariNilaiMax(a, b);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ overloading.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minggu5
+{
+    internal class Overloading
+    {
+        public int CariNilaiMin(int a, int b)
+        {
+            int min;
+            if (a < b)
+            {
+                Console.WriteLine("Nilai min: {0}", min = a);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Nilai min: {0}", min = b);
+                Console.ReadLine();
+            }
+            return min;
+
+        }
+        public int CariNilaiMax(int a, int b)
+        {
+            int max;
+            if (a > b)
+            {
+                Console.WriteLine("Nilai max: {0}", max = a);
+            }
+            else
+            {
+                Console.WriteLine("Nilai max: {0}", max = b);
+            }
+
+            return max;
+        }
+    }
+}
